bc1f62b288aecc033986cba977f8b5720172744c
Francisco Martinho <martinho.francisco@ua.pt>
2022-06-01 19:41:24 +0100

    added order class

--- Order.java
+++ Order.java
@@ -1,3 +1,26 @@
+import java.util.*;
+
 public class Order {
-    
+        private String c_name; //customer name
+        private int c_contact; //customer contact
+        private String shipping_address; 
+        private double grand_total = 0;
+        private Date date; //creation date
+        private List<Item> items = new ArrayList<Item>();
+
+        public Order(String name, int contact, String shipping_address, Date date) {
+                this.c_name = name;
+                this.c_contact = contact;
+                this.shipping_address = shipping_address;
+                this.date = date;
+        }
+
+        public void add_item(Item item){
+                items.add(item);
+                this.grand_total = this.grand_total + item.getCost();
+        }
+
+        public Date getdate(){
+                return this.date;
+        }        
 }
