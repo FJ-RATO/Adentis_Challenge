ba418eb25ce97f6bd1b68cc3acf8a7c424efc8ff
Francisco Martinho <martinho.francisco@ua.pt>
2022-06-01 19:41:00 +0100

    changed the way cost is calculated

--- Item.java
+++ Item.java
@@ -3,13 +3,19 @@ public class Item extends Product{
     private double cost;
     private int shipping_fee;
     private int tax_amount;
+    private int quantity;
 
 
-    public Item(double cost, int shipping_fee, int tax_amount,String name, String category, double weight, double price, Date date) {
+    public Item(int shipping_fee, int tax_amount,int quantity,String name, String category, double weight, double price, Date date) {
         super(name, category, weight, price, date);
-        this.cost = cost;
+        this.cost = price*quantity;
         this.shipping_fee = shipping_fee;
         this.tax_amount = tax_amount;
+        this.quantity = quantity;
+    }
+
+    public int quantity(){
+        return this.quantity;
     }
 
     public double getCost() {
